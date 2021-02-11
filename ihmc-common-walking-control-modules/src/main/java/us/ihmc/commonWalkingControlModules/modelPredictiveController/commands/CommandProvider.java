bd2ef8ec3a673742f7e9f626b6497a09161c10bc
Robert <rgriffin@ihmc.us>
2021-02-10 19:29:24 -0600

    Revert "TO REVERT - deleted some of the remaining orientation things"
    This reverts commit 9ace66f86d078e440795730b11adb03c4d38ac7f.

--- ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/modelPredictiveController/commands/CommandProvider.java
+++ ihmc-common-walking-control-modules/src/main/java/us/ihmc/commonWalkingControlModules/modelPredictiveController/commands/CommandProvider.java
@@ -9,13 +9,19 @@ public class CommandProvider
 {
    private final RecyclingArrayList<CoMPositionCommand> comPositionCommandPool = new RecyclingArrayList<>(CoMPositionCommand::new);
    private final RecyclingArrayList<CoMVelocityCommand> comVelocityCommandPool = new RecyclingArrayList<>(CoMVelocityCommand::new);
+   private final RecyclingArrayList<BodyOrientationCommand> bodyOrientationCommandPool = new RecyclingArrayList<>(BodyOrientationCommand::new);
+   private final RecyclingArrayList<BodyAngularVelocityCommand> bodyAngularVelocityCommandPool = new RecyclingArrayList<>(BodyAngularVelocityCommand::new);
    private final RecyclingArrayList<DCMPositionCommand> dcmPositionCommandPool = new RecyclingArrayList<>(DCMPositionCommand::new);
    private final RecyclingArrayList<DCMVelocityCommand> dcmVelocityCommandPool = new RecyclingArrayList<>(DCMVelocityCommand::new);
    private final RecyclingArrayList<VRPPositionCommand> vrpPositionCommandPool = new RecyclingArrayList<>(VRPPositionCommand::new);
    private final RecyclingArrayList<VRPVelocityCommand> vrpVelocityCommandPool = new RecyclingArrayList<>(VRPVelocityCommand::new);
    private final RecyclingArrayList<VRPTrackingCommand> vrpTrackingCommandPool = new RecyclingArrayList<>(VRPTrackingCommand::new);
+   private final RecyclingArrayList<OrientationTrackingCommand> orientationTrackingCommandPool = new RecyclingArrayList<>(OrientationTrackingCommand::new);
+   private final RecyclingArrayList<OrientationDynamicsCommand> orientationDynamicsCommandPool = new RecyclingArrayList<>(OrientationDynamicsCommand::new);
    private final RecyclingArrayList<CoMPositionContinuityCommand> comPositionContinuityCommandPool = new RecyclingArrayList<>(CoMPositionContinuityCommand::new);
    private final RecyclingArrayList<CoMVelocityContinuityCommand> comVelocityContinuityCommandPool = new RecyclingArrayList<>(CoMVelocityContinuityCommand::new);
+   private final RecyclingArrayList<BodyOrientationContinuityCommand> bodyOrientationContinuityCommandPool = new RecyclingArrayList<>(BodyOrientationContinuityCommand::new);
+   private final RecyclingArrayList<BodyAngularVelocityContinuityCommand> bodyAngularVelocityContinuityCommandPool = new RecyclingArrayList<>(BodyAngularVelocityContinuityCommand::new);
    private final RecyclingArrayList<VRPPositionContinuityCommand> vrpPositionContinuityCommandPool = new RecyclingArrayList<>(VRPPositionContinuityCommand::new);
    private final RecyclingArrayList<RhoValueObjectiveCommand> rhoValueObjectiveCommandPool = new RecyclingArrayList<>(RhoValueObjectiveCommand::new);
 
@@ -26,13 +32,19 @@ public class CommandProvider
    {
       comPositionCommandPool.clear();
       comVelocityCommandPool.clear();
+      bodyOrientationCommandPool.clear();
+      bodyAngularVelocityCommandPool.clear();
       dcmPositionCommandPool.clear();
       dcmVelocityCommandPool.clear();
       vrpPositionCommandPool.clear();
       vrpVelocityCommandPool.clear();
       vrpTrackingCommandPool.clear();
+      orientationTrackingCommandPool.clear();
+      orientationDynamicsCommandPool.clear();
       comPositionContinuityCommandPool.clear();
       comVelocityContinuityCommandPool.clear();
+      bodyOrientationContinuityCommandPool.clear();
+      bodyAngularVelocityContinuityCommandPool.clear();
       vrpPositionContinuityCommandPool.clear();
       rhoValueObjectiveCommandPool.clear();
    }
@@ -45,6 +57,16 @@ public class CommandProvider
    public CoMVelocityCommand getNextCoMVelocityCommand()
    {
       return comVelocityCommandPool.add();
+   }
+
+   public BodyOrientationCommand getNextBodyOrientationCommand()
+   {
+      return bodyOrientationCommandPool.add();
+   }
+
+   public BodyAngularVelocityCommand getNextBodyAngularVelocityCommand()
+   {
+      return bodyAngularVelocityCommandPool.add();
    }
 
    public DCMPositionCommand getNextDCMPositionCommand()
@@ -72,6 +94,16 @@ public class CommandProvider
       return vrpTrackingCommandPool.add();
    }
 
+   public OrientationTrackingCommand getNextOrientationTrackingCommand()
+   {
+      return orientationTrackingCommandPool.add();
+   }
+
+   public OrientationDynamicsCommand getNextOrientationDynamicsCommand()
+   {
+      return orientationDynamicsCommandPool.add();
+   }
+
    public CoMPositionContinuityCommand getNextComPositionContinuityCommand()
    {
       return comPositionContinuityCommandPool.add();
@@ -80,6 +112,16 @@ public class CommandProvider
    public CoMVelocityContinuityCommand getNextComVelocityContinuityCommand()
    {
       return comVelocityContinuityCommandPool.add();
+   }
+
+   public BodyOrientationContinuityCommand getNextBodyOrientationContinuityCommand()
+   {
+      return bodyOrientationContinuityCommandPool.add();
+   }
+
+   public BodyAngularVelocityContinuityCommand getNextBodyAngularVelocityContinuityCommand()
+   {
+      return bodyAngularVelocityContinuityCommandPool.add();
    }
 
    public VRPPositionContinuityCommand getNextVRPPositionContinuityCommand()
